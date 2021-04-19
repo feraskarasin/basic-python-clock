cfb2720f3bb3fc9045a5ecff3bf49a339e115957
feraskarasin <80595285+feraskarasin@users.noreply.github.com>
2021-04-19 23:35:30 +0200

    inital commit

---
+++ Clock PY - main/Clock_PY.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from tkinter.ttk import *
+
+from time import strftime
+
+root = Tk()
+root.title("Clock")
+
+def time():
+    string = strftime('%H:%M:%S-%p')
+    label.config(text=string)
+    label.after(1000, time)
+
+
+label = Label(root, font=("Carre", 80), background = "black", foreground = "white")
+label.pack(anchor='center')
+time()
+
+mainloop()
+
